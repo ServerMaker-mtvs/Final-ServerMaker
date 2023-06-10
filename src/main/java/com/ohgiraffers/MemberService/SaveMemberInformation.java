89e382c95230e92a6d25164f8d20fc55a3868a83
sui <numerical43@daum.net>
2023-06-10 18:22:07 +0900

    입력된 데이터 유효성 검사 기능 완료 #11

--- src/main/java/com/ohgiraffers/MemberService/SaveMemberInformation.java
+++ src/main/java/com/ohgiraffers/MemberService/SaveMemberInformation.java
@@ -1,7 +1,39 @@
 package com.ohgiraffers.MemberService;
 
+import java.text.ParseException;
+
 public class SaveMemberInformation {
+    public void saveInformation (UnCheckedMember data){
+        // 나이 유효성 확인
+        try {
+            CheckMemberBirthday checkMemberBirthday = new CheckMemberBirthday();
+            checkMemberBirthday.checkBirthday(data.unCheckedBirthday);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
 
+        //  비밀번호 유효성 확인
+        try {
+            CheckMemberPwd checkMemberPwd = new CheckMemberPwd();
+            checkMemberPwd.checkPwd(data.unCheckedPassword1, data.unCheckedPassword2);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
 
-    // 수의님
+        // Id 유효성 확인
+        try {
+            CheckMemberId checkMemberId = new CheckMemberId();
+            checkMemberId.checkId(data.unCheckedId);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        // 이름 유효성 확인
+        try {
+            CheckMemberName checkMemberName = new CheckMemberName();
+            checkMemberName.checkName(data.unCheckedName);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 }
