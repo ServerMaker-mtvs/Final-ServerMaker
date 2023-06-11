b7f3ab29421ce059502733926358de05213d1168
LAPTOP-O91Q221D\user <pyunghun120@gmail.com>
2023-06-12 08:33:27 +0900

    [#1 servlet] 회원가입 완료 페이지 보여주기 기능

--- src/main/java/com/ohgiraffers/MemberView/MemberSaveServlet.java
+++ src/main/java/com/ohgiraffers/MemberView/MemberSaveServlet.java
@@ -1,5 +1,46 @@
 package com.ohgiraffers.MemberView;
 
-public class MemberSaveServlet {
-    //평훈 - 이름 꺼내와서 보여주기
+import com.ohgiraffers.Member.MemberDTO;
+import com.ohgiraffers.Member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/himember")
+public class MemberSaveServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    public MemberSaveServlet() {}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+//        List<MemberDTO> members = memberRepository.findAll();
+
+        String memberName = req.getParameter("memberName");
+
+        StringBuilder responseText = new StringBuilder();
+        responseText.append("<!doctype html>\n")
+                .append("<html>\n")
+                .append("<head></head>\n")
+                .append("<body>\n")
+                .append("<a href=\"/index.jsp\">메인</a>")
+                .append("<h3 align=\"center\">")
+                .append(memberName)
+                .append("님 환영합니다!!</h3>")
+                .append("</body>\n")
+                .append("</html>");
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println(responseText.toString());
+
+        out.flush();
+        out.close();
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,13 +5,20 @@
 
 <head></head>
 <body>
-    <h1>ServerMaker - 메인화면</h1>
-    <form action="new-form">
-        <button type="submit"><a href="new-form.jsp" style="text-decoration: none">회원가입</a></button>
-    </form>
-    <br>
-    <form action="members" method="get">
-        <button>회원목록</button>
-    </form>
+    <div class="header" style="background: blanchedalmond; text-align: center" >
+        <h1>ServerMaker</h1>
+    </div>
+    <div class="body" style="text-align: center; background: antiquewhite">
+        <form action="new-form">
+            <button type="submit"><a href="new-form.jsp" style="text-decoration: none">회원가입</a></button>
+        </form>
+        <br>
+        <form action="members" method="get">
+            <button>회원목록</button>
+        </form>
+    </div>
+    <div class="footer" style="background: black; text-align: center; margin-bottom: 0%">
+        <h3>footer</h3>
+    </div>
 </body>
 </html>
