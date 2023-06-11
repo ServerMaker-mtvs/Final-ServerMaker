0b6d63c615fa90a7525438270dc47d2abf48a01e
youyeul lee <yuyeul301@gmail.com>
2023-06-11 20:15:38 +0900

    Date 타입 포맷형식 삭제

--- src/main/java/com/ohgiraffers/MemberService/CheckMemberBirthday.java
+++ src/main/java/com/ohgiraffers/MemberService/CheckMemberBirthday.java
@@ -11,12 +11,12 @@ import java.util.Date;
 public class CheckMemberBirthday {
     public void checkBirth(String date) throws ParseException {
 
-        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd"); //String 으로 넘어온 date 값에 포맷형식을 정해준다.
-        Date parsingDate=sdf.parse(date); //포맷형식이 정해진 값을 parsing 으로 Date 값으로 바꿔준다.
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal=Calendar.getInstance();// Calendar 객체 생성
+        cal.setTime(sdf.parse(date)); //String 타입을 Calendar타입으로 convert 하기 위함
 
-        Calendar cal=Calendar.getInstance(); //Calender 타입 객체 생성(싱글톤)
-        cal.setTime(parsingDate); //Date 타입 시간을 Calendar 시간으로 변경 Date 타입의 내장함수들이 deprecated 상태로 된게 많아서 사용
-        LocalDate now=LocalDate.now();
+
+        LocalDate now=LocalDate.now(); //날짜 까지만 비교하기 떄문에 LocalDate 객체를 사용
         LocalDate paramDate20=LocalDate.of(cal.get(Calendar.YEAR)+19,cal.get(Calendar.MONTH)+1,cal.get(Calendar.DATE));
 
 
