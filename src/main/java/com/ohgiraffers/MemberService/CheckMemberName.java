2843c8115ff1b78aca414e887cf0843950399b5c
jaemin cheong <wjdwoals9292@gmail.com>
2023-06-11 17:14:57 +0900

    checkid, checkname 정상패턴도 걸러내는 버그수정

--- src/main/java/com/ohgiraffers/MemberService/CheckMemberName.java
+++ src/main/java/com/ohgiraffers/MemberService/CheckMemberName.java
@@ -2,26 +2,18 @@ package com.ohgiraffers.MemberService;
 
 public class CheckMemberName {//정재민
     public void checkBlankName(String id){
-        String patternBlank1= "/\\W|\\s/";
-        /*
-          \W 알파벳이나 숫자를 제외한 문자
-          | or
-          \s 공백문자(띄어쓰기)
-          를 포함하는 문자패턴을 검색
-         */
-        if(id==null){
+        //null 처리
+        if(id==null) {
             throw new IllegalArgumentException("이름을 입력해 주세요.");
-        }else if(!id.matches(patternBlank1)){
-            throw new IllegalArgumentException("이름은 공백,특수문자,숫자를 포함할 수 없습니다.");
         }
     }
 
     public void checkPatternName(String id){ //ID 패턴 탐지
-        String pattern1 = "/^[가-힣]{2,30}$/";
+        String pattern1 = "^[가-힣]{2,30}$";
         /*
             /^[가-힣] 가~ 힣 까지 모든 조합된 한글 (ㄱ,ㄴ,ㄷ,ㅏ,ㅑ등 불가능)
-            {2,30}$ 2자~30자 내의
-            문자패턴 검색
+            {2,30}$ 2자~30자 내의 문자패턴
+            특수문자, 공백, 숫자 제외
          */
         if(!id.matches(pattern1)){
             /*
